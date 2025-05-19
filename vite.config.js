223079c6d549e07a2118ee74193c63bcdefbef74
csantander <casdesarrollo.2022@gmail.com>
2025-05-19 16:49:07 -0300

    update

--- vite.config.js
+++ vite.config.js
@@ -1,42 +1,40 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
-import { ViteImageOptimizer } from 'vite-plugin-image-optimizer'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
+import { ViteImageOptimizer } from 'vite-plugin-image-optimizer';
 
 export default defineConfig({
   plugins: [
     react(),
     ViteImageOptimizer({
-      png: {
-        quality: 80,
-      },
-      jpeg: {
-        quality: 80,
-      },
-      jpg: {
-        quality: 80,
-      },
-      webp: {
-        quality: 80,
-      },
-    }),
+      png: { quality: 80 },
+      jpeg: { quality: 80 },
+      jpg: { quality: 80 },
+      webp: { quality: 80 }
+    })
   ],
-  base: '/',
-  server: {
-    host: '0.0.0.0',
-    port: 5174,
-  },
-  css: {
-    postcss: './postcss.config.mjs'
+  resolve: {
+    alias: {
+      'lodash': 'lodash-es' // Fuerza esta redirección
+    }
   },
   build: {
     rollupOptions: {
       output: {
         manualChunks: {
-          // Divide tu bundle en chunks más pequeños
-          react: ['react', 'react-dom'],
-          vendor: ['lodash', 'axios'], // añade otras dependencias grandes aquí
-        },
-      },
-    },
+          react: ['react', 'react-dom', 'react-i18next'],
+          framer: ['framer-motion'],
+          i18n: ['i18next', 'i18next-browser-languagedetector', 'i18next-http-backend'],
+        }
+      }
+    }
+  },
+  // Resto de tu configuración original...
+  base: '/',
+  server: {
+    host: '0.0.0.0',
+    port: 5174
+  },
+  css: {
+    postcss: './postcss.config.mjs'
   }
-})
+});
