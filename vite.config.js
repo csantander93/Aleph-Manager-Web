bac3ee88bdb73691530af1b9eee3f618e0902e8c
csantander <casdesarrollo.2022@gmail.com>
2025-05-19 12:49:11 -0300

    optimizando web mobile

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,42 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import { ViteImageOptimizer } from 'vite-plugin-image-optimizer'
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    ViteImageOptimizer({
+      png: {
+        quality: 80,
+      },
+      jpeg: {
+        quality: 80,
+      },
+      jpg: {
+        quality: 80,
+      },
+      webp: {
+        quality: 80,
+      },
+    }),
+  ],
   base: '/',
   server: {
-    host: '0.0.0.0',   // Esto hace que el servidor escuche en todas las interfaces de red disponibles
-    port: 5174,         // Puedes elegir el puerto que desees
+    host: '0.0.0.0',
+    port: 5174,
   },
   css: {
-    postcss: './postcss.config.mjs' // o .cjs según tu elección
+    postcss: './postcss.config.mjs'
+  },
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          // Divide tu bundle en chunks más pequeños
+          react: ['react', 'react-dom'],
+          vendor: ['lodash', 'axios'], // añade otras dependencias grandes aquí
+        },
+      },
+    },
   }
 })
