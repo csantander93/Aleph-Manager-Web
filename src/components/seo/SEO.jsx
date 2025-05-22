f6a3f6b9c49a54139787610f887953a68408f717
csantander <casdesarrollo.2022@gmail.com>
2025-05-22 20:39:34 -0300

    update logo.svg

--- src/components/seo/SEO.jsx
+++ src/components/seo/SEO.jsx
@@ -18,7 +18,7 @@ const SEO = ({ title, description, keywords, image, url }) => {
   return (
     <Helmet>
       {/* Metadatos básicos */}
-      <title>{`${title} | Aleph Manager`}</title>
+      <title>{`${title}`}</title>
       <meta name="description" content={description} />
       <meta name="keywords" content={keywords} />
       <meta name="robots" content="index, follow" />
